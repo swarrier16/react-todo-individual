6d79dda777055a7872e560b6be27446a2f2462e9
swarrier16 <swarrier16@gmail.com>
2018-04-30 19:44:00 -0400

    transfer files from old repo

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+const addTask =(task)=>{
+    return {
+        type: 'ADD_TASK',
+        payload: task
+    };
+},
+deleteTask =(taskId) =>{
+    return{
+        type:'DELETE_TASK',
+        payload: taskId
+    };
+};
+
+export {addTask, deleteTask};
